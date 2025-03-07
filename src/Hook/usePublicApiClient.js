b8c2b1ad828525fe99d9654d7e0ef985eac199c7
himel1625 <himelmia1625@gmail.com>
2025-03-07 12:25:49 +0600

    feat(api): create custom hook for using public API client

--- src/Hook/usePublicApiClient.js
+++ src/Hook/usePublicApiClient.js
@@ -1,11 +1,8 @@
 import axios from 'axios';
-
 const publicApiClient = axios.create({
     baseURL: 'http://localhost:4004',
 });
-
 const usePublicApiClient = () => {
     return publicApiClient;
 };
-
 export default usePublicApiClient;
